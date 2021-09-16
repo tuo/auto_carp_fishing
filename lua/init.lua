100d2a9b9cfc7fd12343f4c90ff8df89c91e0f69
Tuo <tuo.huang@6e-group.com>
2021-09-16 23:52:37 +0800

    hook up sim800 and mpu6050

--- lua/init.lua
+++ lua/init.lua
@@ -1,17 +1,27 @@
 dofile("sim800c_wrapped.lua")
+dofile("mpu6050_wrapped.lua")
 
-
+tmr.delay(10 * 1000 * 1000) --delay 10 seconds
 
 sim_setup()
 
-tmr.delay(1000 * 1000) --delay one seconds
-sim_send("init")
---
---
+
+
+tmr.delay(1000 * 1000)
+mpu6050_setup();
+
+tmr.delay(1000 * 1000)
+
+data = mpu6050_read()
+sim_send("init:"..data)
+
+print("all done, kick off loop");
+
 mytimer = tmr.create()
 mytimer:register(20000, tmr.ALARM_AUTO, function() 
-    print("hey after 20 seconds") 
-    sim_send("after20")
+    data = mpu6050_read()
+    print("after 20"..data);
+    sim_send("after20: "..data)
 end)
 mytimer:start()
 --
