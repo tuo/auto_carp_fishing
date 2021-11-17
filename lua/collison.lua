f2bcb4c70a931facf19df2db290f065ed95a159e
Tuo <tuo.huang@6e-group.com>
2021-11-17 21:00:54 +0800

    update with YL-99 collision

--- lua/collison.lua
+++ lua/collison.lua
@@ -1,4 +1,4 @@
-pinInput = 3 --D3
+pinInput = 5--D5
 
 pin=4 --D4
 
@@ -13,8 +13,10 @@ while true do   --read and print accelero, gyro and temperature value
 
     if val == gpio.LOW then
         print("low HIT - collided")
+        gpio.write(pin,gpio.LOW)
     else
         print("high")
+        gpio.write(pin,gpio.HIGH)
     end
     tmr.delay(1000)
 --    tmr.delay(1000 * 1000 * 2)   -- 100ms timer delay    
